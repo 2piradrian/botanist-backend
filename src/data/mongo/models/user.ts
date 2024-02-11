f4eff4d3fb98b4c3945680615147ce91b587e9e8
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-02-11 18:13:25 -0300

    user schema

---
+++ src/data/mongo/models/user.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    posts: {
+        type: [String],
+        default: []
+    }
+});
+
+export const UserModel = mongoose.model("User", userSchema);
