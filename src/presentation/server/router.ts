e20d2f58ca4f7a6a1f2d90d2eff7d1dceed0f35a
Adrian Rodriguez <rodriguezcadr@gmail.com>
2024-02-12 14:31:20 -0300

    Add AuthRoutes to the /api/auth endpoint

--- src/presentation/server/router.ts
+++ src/presentation/server/router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
+import { AuthRoutes } from "../app/auth/routes";
 
 export class AppRouter {
     static get routes(): Router {
         const router = Router();
 
-        router.use('/api/auth');
+        router.use('/api/auth', AuthRoutes.routes);
         router.use('/api/posts');
 
         return router;
